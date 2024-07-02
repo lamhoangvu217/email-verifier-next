e6ecf1e1ea91861e3f261c0715950d2e25690efb
lam.vh <lam.vh@dsc.com.vn>
2024-07-02 13:06:07 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { ConfigProvider } from "antd";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
