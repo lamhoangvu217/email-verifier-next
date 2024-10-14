82af73392d4521b52cf85ed9b820bbf0a6e51ae4
lam.vh <lam.vh@dsc.com.vn>
2024-10-14 16:44:23 +0700

    add meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 const MyDocument = () => (
   <Html lang="en">
     <Head>
+      <meta name="google-adsense-account" content={`ca-pub-${process.env.NEXT_PUBLIC_URL_GG_ADS_CODE}`} />
       <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_PUBLIC_URL_GG_ADS_CODE}`} crossorigin="anonymous"></script>
     </Head>
     <body>
