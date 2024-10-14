1df4c2f41fec335a4862ebeaa89af9ad3e4bef8c
lam.vh <lam.vh@dsc.com.vn>
2024-10-14 17:38:24 +0700

    add google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import "@/styles/globals.css";
 import { ConfigProvider } from "antd";
-
+import { GoogleTagManager } from "@next/third-parties/google"
 export default function App({ Component, pageProps }) {
   return (
     <ConfigProvider>
       <Component {...pageProps} />
+      <GoogleTagManager gtmId="GTM-P227RZ4K" />
     </ConfigProvider>
   );
 }
